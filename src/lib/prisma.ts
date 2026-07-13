9bcd11927a8841beb6454a4a5bdaba9709b30017
user <user@MacBook-Pro.local>
2026-07-14 00:27:49 +0700

    trying fix error vercel

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "../../generated/prisma/client/index.js";
 import { Pool } from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { env } from "../config/env.js";
