b91c2eeb44d6dfde5b494473f2dab7a91186d262
user <user@MacBook-Pro.local>
2026-07-15 11:15:50 +0700

    Reinstall swagger 2

--- src/routes/swagger.route.ts
+++ src/routes/swagger.route.ts
@@ -1,10 +1,27 @@
 import { Router } from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerDocument from "../config/doc/swagger-output.json" with { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const router = Router();
 
+const css = fs.readFileSync(
+  path.resolve(__dirname, "../../node_modules/swagger-ui-dist/swagger-ui.css"),
+  "utf-8",
+);
+
 // Serve Swagger documentation
-router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+router.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument, {
+    customCss: css,
+  }),
+);
 
 export default router;
